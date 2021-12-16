339846c580b5c82ff6c27c4ee9cc5c40de4105dd
Paulo César <paulosouza90@gmail.com>
2021-12-15 21:19:40 -0300

    adicionando views genericas

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 
 
-admin.site.register(Question)
+class ListandoQuestion(admin.ModelAdmin):
+    list_display = ('id', 'question_text', 'pub_date', )
+    list_display_links = ('id', 'question_text', )
+
+
+class ListandoChoice(admin.ModelAdmin):
+    list_display = ('id', 'question', 'choice_text', 'votes')
+    list_display_links = ('id', 'choice_text')
+
+
+admin.site.register(Question, ListandoQuestion)
+admin.site.register(Choice, ListandoChoice)
